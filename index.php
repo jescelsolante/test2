d81497a3040c64a8a3b7b5e37f6e076e5679a546
Jesiee <solante.jescel72@gmail.com>
2024-11-06 22:39:23 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>My First Ubuntu Server PHP Deployment</title>
+</head>
+<body>
+	<p>My First Ubuntu Server PHP Deployment</p>
+
+</body>
+</html>
+
